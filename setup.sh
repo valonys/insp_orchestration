4f6c11c361685d45f4760bfe17547984a22bc7de
Ataliba Miguel <ataliba.miguel@valonylabs.com>
2025-06-20 18:22:53 +0100

    Update setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "🔧 Upgrading pip and installing matplotlib..."
-python -m pip install --upgrade pip
-python -m pip install --upgrade matplotlib
-
-echo "📦 Installing remaining requirements..."
-pip install -r requirements.txt
